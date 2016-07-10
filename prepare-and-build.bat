478583f8ad88743720ab4ccb2c9449965d9efc92
DarkCaster <dark.caster@outlook.com>
2016-07-10 14:15:19 +0300

    prepare-and-build.bat - added errorlevel check from powershell script

--- prepare-and-build.bat
+++ prepare-and-build.bat
@@ -5,3 +5,5 @@ PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreferen
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "$ErrorActionPreference='silentlycontinue'; Clear-Content -Path '%ScriptRoot%\service-funcs.ps1' -Stream 'Zone.Identifier'"
 
 PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%ScriptRoot%\prepare-and-build.ps1' %*"
+IF %ERRORLEVEL% NEQ 0 EXIT /B %ERRORLEVEL%
+
