c05c354eb0d2ab902ede236dd15d2658404bf834
DarkCaster <dark.caster@outlook.com>
2017-05-09 23:53:37 +0300

    bash scripts updated

--- build-recipe.sh
+++ build-recipe.sh
@@ -6,17 +6,17 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 . "$script_dir"/service-funcs.sh.in
 
-test "z$recipe" = "z" && log "You must pass recipe file as parameter. This script is not intended to be run as standalone - use prepare-and-build.sh script instead" && exit 1
+[[ -z $recipe ]] && log "You must pass recipe file as parameter. This script is not intended to be run as standalone - use prepare-and-build.sh script instead" && exit 1
 
-test ! -f "$recipe" && log "Recipe file $recipe is not available, exiting" && exit 1
+[[ ! -f $recipe ]] && log "Recipe file $recipe is not available, exiting" && exit 1
 
 log "Loading $recipe recipe"
 
 . "$recipe"
 check_error
 
-test "z$sources" = "z" && log "Recipe does not have src parameter" && exit 1
-test "z$targets" = "z" && log "Recipe does not have tgt parameter" && exit 1
+[[ -z $sources ]] && log "Recipe does not have \"sources\" parameter!" && exit 1
+[[ -z $targets ]] && log "Recipe does not have \"targets\" parameter!" && exit 1
 
 #sources as array
 sources=( $sources )
@@ -35,110 +35,99 @@ cake_exe="$tools_dir/cake/Cake.exe"
 #add tools_dir to PATH
 export PATH="$tools_dir:$PATH"
 
-temp_dir=`mktemp -t build.XXXXXX -d -p "$temp_dir"`
-test ! -d "$temp_dir" && log "Failed to create temp_dir, exiting" && exit 1
+temp_dir=`2>/dev/null mktemp -t build.XXXXXX -d -p "$temp_dir"`
+[[ -z $temp_dir || ! -d $temp_dir ]] && log "Failed to create temp_dir, exiting" && exit 1
 
 cleanup () {
- log "Cleaning up $temp_dir dir"
- rm -rf "$temp_dir"
+  log "Cleaning up $temp_dir dir"
+  rm -rf "$temp_dir"
 }
 
 check_error_cleanup () {
-if [ "$?" != "0" ]; then
- log "Error detected while building $recipe"
- cleanup
- exit 1
-fi
+  if [[ $? != 0 ]]; then
+    log "Error detected while building $recipe"
+    cleanup
+    exit 1
+  fi
 }
 
 #copy extra files and patches
 for el in "${files[@]}"
 do
- log "Copying $el file to build dir"
- cp "$el" "$temp_dir"
- check_error_cleanup
+  log "Copying $el file to build dir"
+  cp "$el" "$temp_dir"
+  check_error_cleanup
 done
 
 #process sources
 for src in "${sources[@]}"
 do
-
-#copy or extract source tree
-if [ -f "$src.zip" ]; then
- log "Extracting $src.zip to build dir"
- unzip -q "$src.zip" -d "$temp_dir"
- check_error_cleanup
-else
-if [ -d "$src" ]; then
- log "Copying $src to build dir"
- cp -r "$src" "$temp_dir"
- check_error_cleanup
-else
- log "$src source not found! exiting"
- cleanup
- exit 1
-fi
-fi
-
-#copy build script
-if [ -f "$src.cake" ]; then
- log "Copying $src.cake file to build dir"
- cp "$src.cake" "$temp_dir/build.cake"
- check_error_cleanup
-else
- log "Cake build script $src.cake not found! exiting"
- cleanup
- exit 1
-fi
-
-#run cake
-olddir="$PWD"
-cd "$temp_dir"
- log "Running $src.cake build script"
- CAKE_PATHS_TOOLS="$tools_dir" mono "$cake_exe"
- if [ "$?" != "0" ]; then
-  log "Build failed!"
+  #copy or extract source tree
+  if [[ -f $src.zip ]]; then
+    log "Extracting $src.zip to build dir"
+    unzip -q "$src.zip" -d "$temp_dir"
+    check_error_cleanup
+  elif [[ -d $src ]]; then
+    log "Copying $src to build dir"
+    cp -r "$src" "$temp_dir"
+    check_error_cleanup
+  else
+    log "$src source not found! exiting"
+    cleanup
+    exit 1
+  fi
+  #copy build script
+  if [[ -f $src.cake ]]; then
+    log "Copying $src.cake file to build dir"
+    cp "$src.cake" "$temp_dir/build.cake"
+    check_error_cleanup
+  else
+    log "Cake build script $src.cake not found! exiting"
+    cleanup
+    exit 1
+  fi
+  #run cake
+  olddir="$PWD"
+  cd "$temp_dir"
+  log "Running $src.cake build script"
+  CAKE_PATHS_TOOLS="$tools_dir" mono "$cake_exe"
+  if [[ $? != 0 ]]; then
+    log "Build failed!"
+    cd "$olddir"
+    cleanup
+    exit 1
+  fi
   cd "$olddir"
-  cleanup
-  exit 1
- fi
-cd "$olddir"
-
 #done processing sources
 done
 
 #create output dir
-if [ ! -d "dist" ]; then
- log "Creating dist dir"
- mkdir -p "dist"
- check_error_cleanup
+if [[ ! -d dist ]]; then
+  log "Creating dist dir"
+  mkdir -p "dist"
+  check_error_cleanup
 fi
 
 #process targets
 for tgt in "${targets[@]}"
 do
-
-#copy target to output dir
-if [ -d "$temp_dir/$tgt" ]; then
- log "Copying $temp_dir/$tgt dir contents to dist dir"
- cp "$temp_dir/$tgt"/* dist
- check_error_cleanup
-else
-if [ -f "$temp_dir/$tgt" ]; then
- log "Copying $temp_dir/$tgt file to dist dir"
- cp "$temp_dir/$tgt" dist
- check_error_cleanup
-else
- log "Build results at $temp_dir/$tgt is not found, exiting"
- cleanup
- exit 1
-fi
-fi
-
+  #copy target to output dir
+  if [[ -d $temp_dir/$tgt ]]; then
+    log "Copying $temp_dir/$tgt dir contents to dist dir"
+    cp "$temp_dir/$tgt"/* dist
+    check_error_cleanup
+  elif [[ -f $temp_dir/$tgt ]]; then
+    log "Copying $temp_dir/$tgt file to dist dir"
+    cp "$temp_dir/$tgt" dist
+    check_error_cleanup
+  else
+    log "Build results at $temp_dir/$tgt is not found, exiting"
+    cleanup
+    exit 1
+  fi
 #done processing targets
 done
 
 log "Build of $recipe recipe is complete"
 cleanup
 exit 0
-
--- prepare-and-build.sh
+++ prepare-and-build.sh
@@ -9,15 +9,15 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 #clear log
 clear_log
 
-if [ "z$recipes_dir" = "z" ]; then
- log "recipes dir not specified. Only preparing build tools and env."
+if [[ -z $recipes_dir ]]; then
+  log "recipes dir not specified. Only preparing build tools and env."
 else
- if [ -d "$recipes_dir" ]; then
-  log "Attempting to build recipes at $recipes_dir"
- else
-  log "Error: recipes dir at $recipes_dir does not exist"
-  exit 1
- fi
+  if [[ -d $recipes_dir ]]; then
+    log "Attempting to build recipes at $recipes_dir"
+  else
+    log "Error: recipes dir at $recipes_dir does not exist"
+    exit 1
+  fi
 fi
 
 #temp dir
@@ -33,58 +33,52 @@ cake_dir="$tools_dir/cake"
 cake_exe="$cake_dir/Cake.exe"
 cake_dist="$script_dir/dist/cake/Cake-bin-net45-v0.19.5.zip"
 
-if [ ! -d "$temp_dir" ]; then
- log "Creating temp dir at $temp_dir"
- mkdir -p $temp_dir
- check_error
+if [[ ! -d $temp_dir ]]; then
+  log "Creating temp dir at $temp_dir"
+  mkdir -p $temp_dir
+  check_error
 fi
 
-if [ ! -d "$tools_dir" ]; then
- log "Creating tools dir at $tools_dir"
- mkdir -p $tools_dir
- check_error
+if [[ ! -d $tools_dir ]]; then
+  log "Creating tools dir at $tools_dir"
+  mkdir -p $tools_dir
+  check_error
 fi
 
-if [ ! -f "$nuget_exe" ]; then
- log "Creating nuget dir at $nuget_dir"
- mkdir -p $nuget_dir
- check_error
- log "Copying nuget exe from $nuget_exe_dist"
- cp "$nuget_exe_dist" "$nuget_dir/nuget.exe"
- check_error
+if [[ ! -f $nuget_exe ]]; then
+  log "Creating nuget dir at $nuget_dir"
+  mkdir -p $nuget_dir
+  check_error
+  log "Copying nuget exe from $nuget_exe_dist"
+  cp "$nuget_exe_dist" "$nuget_exe"
+  check_error
 fi
 
 
-if [ ! -f "$cake_exe" ]; then
- log "Creating cake dir at $cake_dir"
- mkdir -p $cake_dir
- check_error
- log "Extracting cake dist from $cake_dist"
- unzip -q "$cake_dist" -d "$cake_dir"
- check_error
+if [[ ! -f $cake_exe ]]; then
+  log "Creating cake dir at $cake_dir"
+  mkdir -p $cake_dir
+  check_error
+  log "Extracting cake dist from $cake_dist"
+  unzip -q "$cake_dist" -d "$cake_dir"
+  check_error
 fi
 
-if [ "z$recipes_dir" = "z" ]; then
- log "Exiting"
- exit 0
+if [[ -z $recipes_dir ]]; then
+  log "Exiting"
+  exit 0
 fi
 
-olddir="$PWD"
 cd "$recipes_dir"
 check_error
 
-while read recipe
+for recipe in *.recipe
 do
- test "z$recipe" = "z" && break
- log "Processing $recipe"
- "$script_dir/build-recipe.sh" "$recipe"
- if [ "z$?" != "z0" ]; then
-  log "Failed to build $recipe recipe"
-  cd "$olddir"
-  exit 1
- fi
-done <<< "$(find * -maxdepth 1 -iname '*.recipe' -type f | sort)"
-
-cd "$olddir"
-check_error
-
+  [[ ! -f $recipe ]] && break
+  log "Processing $recipe"
+  "$script_dir/build-recipe.sh" "$recipe"
+  if [[ $? != 0 ]]; then
+    log "Failed to build $recipe recipe"
+    exit 1
+  fi
+done
